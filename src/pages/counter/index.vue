4fef2683fbb7b00a26834e84ab5ab9e01ac1b3b7
walljs <walljs@163.com>
2018-07-14 23:56:17 +0800

    initialize

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+
+    <a href="/pages/index/main" class="home">去往首页</a>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store';
+
+export default {
+  computed: {
+    count() {
+      return store.state.count;
+    },
+  },
+  methods: {
+    increment() {
+      store.commit('increment');
+    },
+    decrement() {
+      store.commit('decrement');
+    },
+    handleClick() {
+      console.log('click me!!!!');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+  background-color: red;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
